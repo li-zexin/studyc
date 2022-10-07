86fdf249f7eb30afb80fe4838d9d66d06aee60a3
lizexin <amzexin@163.com>
2022-10-07 22:13:50 +0800

    精简string-endchar-test用例 新增common-function.c/numberDigits

---
+++ src/string-endchar-test.c
@@ -0,0 +1,91 @@
+/*
+ * string-endchar-test.c
+ *
+ *  Created on: 2022年10月7日
+ *      Author: zexin.li
+ */
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int numberDigits(int num);
+
+/**
+ * 打印字符数组
+ */
+void printfCharArray(const char *caseName, char charArray[], int arrayLength) {
+	printf("%s\t\t%d\t\t\"%s\"\t\t\t[", caseName, arrayLength, charArray);
+	for (int i = 0; i < arrayLength; i++) {
+		if (i == 0) {
+			printf("%c", charArray[i]);
+		} else {
+			printf(", %c", charArray[i]);
+		}
+	}
+	printf("]\n");
+}
+
+/**
+ * 结束符：0、\0、NULL
+ */
+void case0() {
+	char endChar[] = { '\0', 0, NULL };
+	int length = sizeof(endChar) / sizeof(endChar[0]);
+	for (int i = 0; i < length; i++) {
+		char str[4] = { 'a', '0', endChar[i], 'b' };
+		int length = sizeof(str) / sizeof(str[0]);
+		char *caseName = malloc(strlen(__FUNCTION__) + 1 + numberDigits(i) + 1);
+		sprintf(caseName, "%s-%d", __FUNCTION__, i);
+		printfCharArray(caseName, str, length);
+	}
+}
+
+/**
+ * 没有结束符，打印乱码
+ */
+void case1() {
+	char str[] = { 'a', '0', 'b' };
+	int length = sizeof(str) / sizeof(str[0]);
+	printfCharArray(__FUNCTION__, str, length);
+}
+
+/*
+ * 双引号引用的字符串，不指定字符数组初始长度
+ * 猜测：底层在把“a”赋值给str数组时，判断已经是一个数组就没有将“b”赋值给str数组了
+ * 所以：字符一定要用单引号
+ */
+void case2() {
+	char str[] = { "a", "0", "b" };
+	int length = sizeof(str) / sizeof(str[0]);
+	printfCharArray(__FUNCTION__, str, length);
+
+}
+
+/**
+ * 双引号引用的字符串，指定字符数组初始长度
+ */
+void case3() {
+	char str[6] = { "a", "0", "b" };
+	int length = sizeof(str) / sizeof(str[0]);
+	printfCharArray(__FUNCTION__, str, length);
+}
+
+/**
+ * 数组中间含有结束符，但是故意打印后续字符是仍旧可以继续打印的
+ */
+void case4() {
+	char str[4] = { 'a', '\0', 'b', NULL };
+	int length = sizeof(str) / sizeof(str[0]);
+	printfCharArray(__FUNCTION__, str, length);
+}
+
+void testString() {
+	printf("=======>>>%s<<<========\n", __FILE__);
+	printf("用例编号\t\t数组长度\t\t字符串\t\t\t数组\n");
+	case0();
+	case1();
+	case2();
+	case3();
+	case4();
+
+}
